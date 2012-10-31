9349c0d0be6820235d4afd16186e04ad66164b6b
Matthew Daiter <mdaiter@exeter.edu>
2012-10-31 01:39:35 -0400

    Removed files

--- compileAndRun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-dmd main.d initSetup.d fileReader.d install/mainInstaller.d install/buildHowTo.d upload/mainUpload.d remove/remove.d install/commonBuildTools.d install/packageStripper.d update/updater.d author/author.d -O -release -ofarpak
-cp arpak /usr/local/bin/
